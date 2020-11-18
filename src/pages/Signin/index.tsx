a9037d860bce8e34f95d02c1b7f93d33b1e5ed78
Roger Rezende <javielrezende@gmail.com>
2020-11-18 17:08:04 -0300

    :sparkles: Add basic context to get auth infos
    Add basic context to get auth infos

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useContext, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -9,12 +9,17 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
+import AuthContext from '../../context/AuthContext';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const auth = useContext(AuthContext);
+  console.log(auth);
 
   const handleSubmit = useCallback(async (data: any) => {
     try {
